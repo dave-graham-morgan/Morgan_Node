76d8fe564d2bb6030312debb60f54adb82aaa284
Dave Morgan <dave.g.morgan@gmail.com>
2023-12-12 17:48:16 -0700

    markov project

--- node-markov/makeText.js
+++ node-markov/makeText.js
@@ -1,2 +1,56 @@
 /** Command-line tool to generate Markov text. */
 
+const fs = require('fs');
+const util = require('util');
+const readFileAsync = util.promisify(fs.readFile);
+
+const axios = require('axios');
+const markov = require('./markov.js');
+
+async function start(inputType){
+   let fileContents = '';
+   if (inputType === 'file'){
+      fileContents = await ingestFile()
+
+   }else if (inputType ==='url'){
+      fileContents = await ingestURL()
+   }else{
+      console.error("Something went wrong! That is not a valid type")
+      process.exit(1)
+   }
+   const mm = new markov.MarkovMachine(fileContents)
+   const stringArray = mm.makeText();
+   console.log(stringArray.join(' '))
+   console.log("and we're donzo.")
+}
+
+async function ingestURL(){
+   let url = process.argv[3]
+   try{
+      let response = await axios.get(url)
+      return response.data
+   }catch (err){
+      console.error(`something went wrong ${err}`)
+      process.exit(1)
+   }
+}
+
+async function ingestFile(){
+   //ingest a file and pass to markov.js
+   let path = process.argv[3]
+   try{
+      fs.accessSync(path, fs.constants.F_OK);
+      const data = await readFileAsync(path, 'utf8');
+      console.log("success Davey!")
+      return data;
+
+   }catch(err){
+      console.error(`file not found: ${err}`);
+      process.exit(1)
+   }
+   
+}
+
+const inputType = process.argv[2]
+console.log(`inputType is ${inputType}`)
+start(inputType)
--- node-markov/markov.js
+++ node-markov/markov.js
@@ -8,22 +8,68 @@ class MarkovMachine {
   constructor(text) {
     let words = text.split(/[ \r\n]+/);
     this.words = words.filter(c => c !== "");
-    this.makeChains();
+    this.markovChain = this.makeChains(); 
   }
-
-  /** set markov chains:
-   *
-   *  for text of "the cat in the hat", chains will be
-   *  {"the": ["cat", "hat"], "cat": ["in"], "in": ["the"], "hat": [null]} */
 
   makeChains() {
-    // TODO
+    const mvChain = {}
+    for (let i = 0; i<this.words.length;i++){
+      let currWord = this.words[i]
+      let nextWord = this.words[i+1]
+
+      //check if currWord is already a key in the object, if it is
+      //add nextword to the value array
+      if (mvChain.hasOwnProperty(currWord)){
+        if(nextWord){
+          mvChain[currWord].push(nextWord)
+        }else{
+          //check we're at the end and exit if we are
+          mvChain[currWord].push(null) 
+          continue;
+        }
+        
+      }else{
+        //first time we're seeing this word so create a key and an array with next word
+        if(nextWord){
+          mvChain[currWord] = [nextWord]
+        }else{
+          mvChain[currWord] = [null];
+          continue;
+        }
+        
+      }
+    }
+    return mvChain
   }
-
-
   /** return random text from chains */
 
   makeText(numWords = 100) {
-    // TODO
+    //randomly select a start word out of our words array
+    let firstWord  = this.getRandomWord(); 
+    let text = [firstWord];
+    while (text.length <= numWords){
+      let nextWord = this.getNextWord(text[text.length-1])
+      if(nextWord){
+        text.push(nextWord);
+      }else{
+        text.push('.');
+        text.push(this.getRandomWord())
+      }
+    }
+    
+    return text
   }
+  getRandomWord(){
+    return this.words[Math.floor(Math.random()*this.words.length)];
+  }
+
+  getNextWord(word){
+    let randomValue = Math.floor(Math.random()* this.markovChain[word].length)
+    return this.markovChain[word][randomValue]
+  }
+}
+
+
+module.exports = {
+  MarkovMachine,
 }
