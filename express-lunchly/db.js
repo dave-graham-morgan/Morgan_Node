8f8eff5c6ddc38b225e3c95ccec49226b6a1623d
Dave Morgan <dave.g.morgan@gmail.com>
2024-01-15 15:20:07 -0700

    completed lunchly exercises

---
+++ express-lunchly/db.js
@@ -0,0 +1,28 @@
+/** Database for lunchly */
+
+const pg = require("pg");
+
+let DB_URI;
+
+if (process.env.NODE_ENV === 'test'){
+   DB_URI = 'postgresql:///lunchly_test';
+}else{
+   DB_URI = 'postgresql:///lunchly';
+}
+
+let db = new pg.Client({
+   connectionString: DB_URI
+})
+
+async function connectToDB(){
+   try{
+      await db.connect();
+      const now = new Date();
+      console.log("Connected to db at",now)
+   }catch(e){
+      console.error("error connecting to db", e)
+   }
+}
+connectToDB();
+
+module.exports = db;
---
+++ express-lunchly/server.js
@@ -0,0 +1,7 @@
+/** Start server for Lunchly. */
+
+const app = require("./app");
+
+app.listen(8080, function() {
+  console.log("listening on 8080");
+});
