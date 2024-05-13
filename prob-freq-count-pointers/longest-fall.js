126b1db36c7ea73167fe0102286bf8ecaace522d
Dave Morgan <dave.g.morgan@gmail.com>
2024-05-13 16:46:26 -0600

    completed one of two extra credit prob freq counters

--- prob-freq-count-pointers/longest-fall.js
+++ prob-freq-count-pointers/longest-fall.js
@@ -1,3 +1,17 @@
 // add whatever parameters you deem necessary
-function longestFall() {
+function longestFall(array) {
+    if (array.length === 0) return 0;
+    let answer = 1;
+    let iterAnswer = 1;
+    for (let i = 0; i<array.length;i++){
+        if(array[i]>array[i+1]){
+            iterAnswer ++;
+        }else{
+            if(iterAnswer>answer){
+                answer = iterAnswer;
+            }
+            iterAnswer = 1;
+        }
+    }
+    return answer;
 }
