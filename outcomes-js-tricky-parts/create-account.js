ad13d579bbea5a03c2a3c3b2cf6a09a84cf7f967
Dave Morgan <dave.g.morgan@gmail.com>
2024-05-14 18:20:19 -0600

    tricky parts concluded

--- outcomes-js-tricky-parts/create-account.js
+++ outcomes-js-tricky-parts/create-account.js
@@ -1,5 +1,29 @@
-function createAccount(pin, amount) {
+function createAccount(pin, amount=0) {
+    let currentPin = pin;
+    let balance = amount;
 
+    return {
+        checkBalance(pin){
+            if(pin !== currentPin) return "Invalid PIN.";
+            return `$${balance}`;
+        },
+        deposit(pin, amt){
+            if(pin !== currentPin) return "Invalid PIN.";
+            balance = balance + amt;
+            return `Successfully deposited $${amt}. Current balance: $${balance}.`;
+        },
+        withdraw(pin, amt){
+            if(pin !== currentPin) return "Invalid PIN.";
+            if(amt > balance) return "Withdrawal amount exceeds account balance. Transaction cancelled.";
+            balance = balance - amt;
+            return `Successfully withdrew $${amt}. Current balance: $${balance}.`
+        },
+        changePin(oldPin, newPin){
+            if(oldPin !== currentPin) return "Invalid PIN.";
+            currentPin = newPin;
+            return "PIN successfully changed!";
+        }
+    }
 }
 
 module.exports = { createAccount };
--- outcomes-js-tricky-parts/guessing-game.js
+++ outcomes-js-tricky-parts/guessing-game.js
@@ -1,5 +1,26 @@
 function guessingGame() {
+    const newRandomNumber = 60; //Math.floor(Math.random()) * 100;
+    let gameWon = false;
+    let count = 1;
 
+    return function(guess){
+        if(gameWon) return "The game is over, you already won!";
+
+        if(guess < 0 || guess > 99){
+            return "please enter a valid number between 1 and 99";
+            count++;
+        }
+        if(guess < newRandomNumber){
+            count ++;
+            return `${guess} is too low!`;
+        }else if(guess > newRandomNumber){
+            count ++;
+            return  `${guess} is too high!`;
+        }else{
+            gameWon = true;
+            return `You win! You found ${guess} in ${count} guesses.`;
+        }
+    }
 }
 
 module.exports = { guessingGame };
